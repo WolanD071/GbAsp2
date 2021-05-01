e45b5a90e721d0c5ee7fa23254691c75464ce5a5
WolanD071 <75041943+WolanD071@users.noreply.github.com>
2021-05-01 21:42:30 +0300

    GbWebApp.Clients project added, ValuesController, ValuesClient

---
+++ Services/GbWebApp.Clients/Values/ValuesClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Linq;
+using System.Net.Http;
+using GbWebApp.Clients.Base;
+using System.Collections.Generic;
+using GbWebApp.Interfaces.TestAPI;
+using Microsoft.Extensions.Configuration;
+
+namespace GbWebApp.Clients.Values
+{
+    public class ValuesClient : BaseClient, IValuesService
+    {
+        public ValuesClient(IConfiguration Configuration) : base(Configuration, "api/values") { }
+
+        public IEnumerable<string> Get()
+        {
+            var response = Http.GetAsync(Address).Result;
+            return response.IsSuccessStatusCode ? response.Content.ReadAsAsync<IEnumerable<string>>().Result
+                : Enumerable.Empty<string>();
+        }
+
+        public string Get(int id)
+        {
+            var response = Http.GetAsync($"{Address}/{id}").Result;
+            return response.IsSuccessStatusCode ? response.Content.ReadAsAsync<string>().Result : string.Empty;
+        }
+
+        public Uri Create(string value)
+        {
+            var response = Http.PostAsJsonAsync(Address, value).Result;
+            return response.EnsureSuccessStatusCode().Headers.Location;
+        }
+
+        public HttpStatusCode Edit(int id, string value)
+        {
+            var response = Http.PutAsJsonAsync($"{Address}/{id}", value).Result;
+            return response.EnsureSuccessStatusCode().StatusCode;
+        }
+
+        public bool Remove(int id)
+        {
+            var response = Http.DeleteAsync($"{Address}/{id}").Result;
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ Services/GbWebApp.Interfaces/TestAPI/IValuesService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+
+namespace GbWebApp.Interfaces.TestAPI
+{
+    public interface IValuesService
+    {
+        IEnumerable<string> Get();
+
+        string Get(int id);
+
+        Uri Create(string value);
+
+        HttpStatusCode Edit(int id, string value);
+
+        bool Remove(int id);
+    }
+}
