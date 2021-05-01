f7faa2f9d392a5fe52615db5eec86d158a13f10d
WolanD071 <75041943+WolanD071@users.noreply.github.com>
2021-05-02 00:15:45 +0300

    web api testing in main app

---
+++ UI/GbWebApp/Controllers/WebAPIController.cs
@@ -0,0 +1,14 @@
+﻿using GbWebApp.Interfaces.TestAPI;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GbWebApp.Controllers
+{
+    public class WebAPIController : Controller
+    {
+        private readonly IValuesService __valuesService;
+
+        public WebAPIController(IValuesService valuesService) => __valuesService = valuesService;
+
+        public IActionResult Index() => View(__valuesService.Get());
+    }
+}
