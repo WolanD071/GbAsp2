b7e5f3e4afc9cf48179427f73f987c03b68d93d5
WolanD071 <75041943+WolanD071@users.noreply.github.com>
2021-05-02 20:19:32 +0300

    web api client essential functionality at the online-shop site... but Http.PostAsJsonAsync() DOESN'T WORK!!!

--- UI/GbWebApp/Controllers/WebAPIController.cs
+++ UI/GbWebApp/Controllers/WebAPIController.cs
@@ -1,4 +1,6 @@
-﻿using GbWebApp.Interfaces.TestAPI;
+﻿using GbWebApp.Domain.Entities.Identity;
+using GbWebApp.Interfaces.TestAPI;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GbWebApp.Controllers
@@ -10,5 +12,41 @@ namespace GbWebApp.Controllers
         public WebAPIController(IValuesService valuesService) => __valuesService = valuesService;
 
         public IActionResult Index() => View(__valuesService.Get());
+
+        public IActionResult GetById(int id)
+        {
+            ViewBag.Id = id;
+            return View(model: __valuesService.Get(id));
+        }
+
+        [HttpGet]
+        [Authorize(Roles = Role.Admin)]
+        public IActionResult NewVal() => View();
+
+        [HttpPost]
+        [Authorize(Roles = Role.Admin)]
+        public IActionResult NewVal([FromForm] string value)
+        {
+            __valuesService.Create(value);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Authorize(Roles = Role.Admin)]
+        public IActionResult DelById(int id)
+        {
+            ViewBag.Id = id;
+            return View(model: __valuesService.Get(id));
+        }
+
+        [HttpPost]
+        [Authorize(Roles = Role.Admin)]
+        public IActionResult DeleteValue(int id)
+        {
+            if (__valuesService.Get(id) == string.Empty)
+                return NotFound();
+            __valuesService.Remove(id);
+            return RedirectToAction("Index");
+        }
     }
 }
