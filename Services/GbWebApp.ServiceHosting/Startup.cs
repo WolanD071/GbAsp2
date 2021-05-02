c4328770f4e38032100b127fe168ca1a43735d66
WolanD071 <75041943+WolanD071@users.noreply.github.com>
2021-05-02 20:50:23 +0300

    copy necessary services to startup.cs of ServiceHosting app

--- Services/GbWebApp.ServiceHosting/Startup.cs
+++ Services/GbWebApp.ServiceHosting/Startup.cs
@@ -1,7 +1,14 @@
+using GbWebApp.DAL.Context;
+using GbWebApp.Domain.Entities.Identity;
+using GbWebApp.Interfaces.Services;
+using GbWebApp.Services.Data;
+using GbWebApp.Services.Services.InDB;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
@@ -15,6 +22,34 @@ namespace GbWebApp.ServiceHosting
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<GbWebAppDB>(opt => opt.UseSqlServer(Configuration.GetConnectionString("Default")));
+            services.AddTransient<AppDBInitializer>();
+
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<GbWebAppDB>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(opt =>
+            {
+#if DEBUG
+                opt.Password.RequiredLength = 3;
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredUniqueChars = 3;
+#endif
+                opt.User.RequireUniqueEmail = false;
+                opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_";
+                opt.Lockout.AllowedForNewUsers = false;
+                opt.Lockout.MaxFailedAccessAttempts = 5;
+                opt.Lockout.DefaultLockoutTimeSpan = System.TimeSpan.FromMinutes(5);
+            });
+
+            services.AddTransient(typeof(IAnyEntityCRUD<>), typeof(InDbAnyEntity<>));
+            services.AddTransient<IProductData, InDbProductData>();
+            services.AddTransient<IOrderService, InDbOrdertData>();
+
             services.AddControllers();
             services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "GbWebApp.ServiceHosting", Version = "v1" }));
         }
