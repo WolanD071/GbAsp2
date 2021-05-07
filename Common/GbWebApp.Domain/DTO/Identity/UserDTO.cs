e3b917a8b1e34f8512f241dadbbec39386117e8c
WolanD071 <75041943+WolanD071@users.noreply.github.com>
2021-05-07 22:49:34 +0300

    Users/Roles interfaces and DTO-models

---
+++ Common/GbWebApp.Domain/DTO/Identity/UserDTO.cs
@@ -0,0 +1,26 @@
+﻿using GbWebApp.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace GbWebApp.Domain.DTO.Identity
+{
+    public abstract class UserDTO
+    {
+        public User User { get; set; }
+    }
+
+    public class AddLoginDTO : UserDTO
+    {
+        public UserLoginInfo UserLoginInfo { get; set; }
+    }
+
+    public class PasswordHashDTO : UserDTO
+    {
+        public string Hash { get; set; }
+    }
+
+    public class SetLockoutDTO : UserDTO
+    {
+        public DateTimeOffset? LockoutEnd { get; set; }
+    }
+}
---
+++ Services/GbWebApp.Interfaces/Services/Identity/IRolesService.cs
@@ -0,0 +1,7 @@
+﻿using GbWebApp.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace GbWebApp.Interfaces.Services.Identity
+{
+    public interface IRolesService : IRoleStore<Role> { }
+}
