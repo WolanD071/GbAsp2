97773ccb4a2bfe574c27082d94c24a19be2fde83
WolanD071 <75041943+WolanD071@users.noreply.github.com>
2021-05-07 23:44:34 +0300

    Users/Roles clients

---
+++ Services/GbWebApp.Clients/Identity/RolesClient.cs
@@ -0,0 +1,81 @@
+﻿using System.Net.Http;
+using System.Threading;
+using GbWebApp.Interfaces;
+using GbWebApp.Clients.Base;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using GbWebApp.Domain.Entities.Identity;
+using Microsoft.Extensions.Configuration;
+using GbWebApp.Interfaces.Services.Identity;
+
+namespace GbWebApp.Clients.Identity
+{
+    public class RolesClient : BaseClient, IRolesService
+    {
+        public RolesClient(IConfiguration configuration) : base(configuration, WebApiRoutes.Identity.Role) { }
+
+        public async Task<IdentityResult> CreateAsync(Role role, CancellationToken cancel)
+        {
+            var response = await PostAsync(Address, role, cancel).ConfigureAwait(false);
+            return await response.Content.ReadAsAsync<bool>(cancel)
+                ? IdentityResult.Success
+                : IdentityResult.Failed();
+        }
+
+        public async Task<IdentityResult> UpdateAsync(Role role, CancellationToken cancel)
+        {
+            var response = await PutAsync(Address, role, cancel).ConfigureAwait(false);
+            return await response.Content.ReadAsAsync<bool>(cancel)
+                ? IdentityResult.Success
+                : IdentityResult.Failed();
+        }
+
+        public async Task<IdentityResult> DeleteAsync(Role role, CancellationToken cancel)
+        {
+            var response = await PostAsync($"{Address}/Delete", role, cancel).ConfigureAwait(false);
+            return await response.Content.ReadAsAsync<bool>(cancel)
+                ? IdentityResult.Success
+                : IdentityResult.Failed();
+        }
+
+        public async Task<string> GetRoleIdAsync(Role role, CancellationToken cancel)
+        {
+            var response = await PostAsync($"{Address}/GetRoleId", role, cancel).ConfigureAwait(false);
+            return await response.Content.ReadAsAsync<string>(cancel);
+        }
+
+        public async Task<string> GetRoleNameAsync(Role role, CancellationToken cancel)
+        {
+            var response = await PostAsync($"{Address}/GetRoleName", role, cancel).ConfigureAwait(false);
+            return await response.Content.ReadAsAsync<string>(cancel);
+        }
+
+        public async Task SetRoleNameAsync(Role role, string name, CancellationToken cancel)
+        {
+            var response = await PostAsync($"{Address}/SetRoleName/{name}", role, cancel).ConfigureAwait(false);
+            role.Name = await response.Content.ReadAsAsync<string>(cancel);
+        }
+
+        public async Task<string> GetNormalizedRoleNameAsync(Role role, CancellationToken cancel)
+        {
+            var response = await PostAsync($"{Address}/GetNormalizedRoleName", role, cancel).ConfigureAwait(false);
+            return await response.Content.ReadAsAsync<string>(cancel);
+        }
+
+        public async Task SetNormalizedRoleNameAsync(Role role, string name, CancellationToken cancel)
+        {
+            var response = await PostAsync($"{Address}/SetNormalizedRoleName/{name}", role, cancel).ConfigureAwait(false);
+            role.NormalizedName = await response.Content.ReadAsAsync<string>(cancel);
+        }
+
+        public async Task<Role> FindByIdAsync(string id, CancellationToken cancel)
+        {
+            return await GetAsync<Role>($"{Address}/FindById/{id}", cancel).ConfigureAwait(false);
+        }
+
+        public async Task<Role> FindByNameAsync(string name, CancellationToken cancel)
+        {
+            return await GetAsync<Role>($"{Address}/FindByName/{name}", cancel).ConfigureAwait(false);
+        }
+    }
+}
