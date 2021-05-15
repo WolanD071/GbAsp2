7aa266b73790682801675d50de3e1f54ab59ae5d
WolanD071 <75041943+WolanD071@users.noreply.github.com>
2021-05-16 00:25:51 +0300

    simple way of main menu links to underline and new client/controller (blogpost) adding

--- Services/GbWebApp.Interfaces/WebApiRoutes.cs
+++ Services/GbWebApp.Interfaces/WebApiRoutes.cs
@@ -3,6 +3,7 @@
     public static class WebApiRoutes
     {
         public const string EmployeesAPI = "api/employees";
+        public const string BlogPostsAPI = "api/blogposts";
         public const string ProductsAPI = "api/products";
         public const string TestWebAPI = "api/values";
         public const string OrdersAPI = "api/orders";
